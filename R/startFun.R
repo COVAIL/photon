4c8db4b8f42a609193151b205cf818183c4a8bd6
Abbas Rizvi <arizvi@MBP2018-004.local>
2019-06-11 13:13:15 -0400

    start Fun

--- R/startFun.R
+++ R/startFun.R
@@ -7,16 +7,16 @@ startFun <- function(input_path, cran_packages=NULL, bioc_packages=NULL, github_
   message("The directory is ", input_path)
   input_path <- sprintf("%s/electron-quick-start", input_path) 
   #confirm versions greater than (node 8.4.0 and npm 5.3)
-  nodeVersion <- system2("node", args="-v", stdout=TRUE, stderr=TRUE)
-  npmVersion <- system2("npm", args="-v", stdout=TRUE, stderr=TRUE)
+  nodeVersion <- system2("node", args="-v", stdout=TRUE, stderr=TRUE, wait=TRUE, env=environment())
+  npmVersion <- system2("npm", args="-v", stdout=TRUE, stderr=TRUE, wait=TRUE, env=environment())
   #if node not available, notify to install node and npm globally 
   
-  electronPackagerVersion <- system2("npm", args="list -g electron-packager" , stdout=TRUE, stderr=TRUE)
+  electronPackagerVersion <- system2("npm", args="list -g electron-packager" , stdout=TRUE, stderr=TRUE, wait=TRUE, env=environment())
   
   if(grepl("electron", electronPackagerVersion[2])){
     electronPackagerVersion <- str_extract(electronPackagerVersion[2], "[0-9]+\\.[0-9]+\\.[0-9]+")
   } else{
-    system2("npm", args='install electron-packager -g', stdout=TRUE, stderr = TRUE)
+    system2("npm", args='install electron-packager -g', stdout=TRUE, stderr = TRUE, wait=TRUE, env=environment())
     electronPackagerVersion <- str_extract(electronPackagerVersion[2], "[0-9]+\\.[0-9]+\\.[0-9]+")
   }
   
@@ -59,7 +59,7 @@ startFun <- function(input_path, cran_packages=NULL, bioc_packages=NULL, github_
     
     
     r_electron_version <- system(sprintf("cd %s; ./R CMD BATCH --version", r_portable_path),
-                                 intern = TRUE)[5]
+                                 intern = TRUE, wait=TRUE)[5]
     
     r_electron_version <- stringr::str_extract(r_electron_version, "[0-9]+\\.[0-9]+\\.[0-9]+")
     
@@ -88,7 +88,8 @@ startFun <- function(input_path, cran_packages=NULL, bioc_packages=NULL, github_
         cran_packages,
         github_packages,
         bioc_packages
-      )
+      ), wait=TRUE
+      
     )
     
     
