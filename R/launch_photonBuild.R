0b70e05ea4610d03a050a5df5b1a38e1f18fd4e9
Abbas Rizvi <arizvi@MBP2018-004.local>
2019-06-11 11:52:38 -0400

    moving stuff around

--- R/launch_photonBuild.R
+++ R/launch_photonBuild.R
@@ -3,11 +3,10 @@
 #' @import shiny
 #' @export
 
-launch_photoBuild <- function(launch.browser = TRUE, ...) {
+launch_photoBuild <- function() {
   fpath <- system.file("application", package = "photon")
   # Use a modal dialog as a viewr.
-  viewer <- shiny::dialogViewer("Photon Shiny App Builder", width = 700, height = 800)
   #viewer <- shiny::paneViewer()
-  shiny::runGadget(ui, server, viewer = shiny::paneViewer(), ...)
-  }
+  shiny::runGadget(shinyApp(fpath), viewer = shiny::paneViewer())
+}
 
