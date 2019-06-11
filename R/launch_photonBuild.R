4f26f5463ca23a33044ed154ebaab57f576d821a
Abbas Rizvi <arizvi@MBP2018-004.local>
2019-06-11 11:37:32 -0400

    export launch photon

--- R/launch_photonBuild.R
+++ R/launch_photonBuild.R
@@ -1,6 +1,7 @@
 #' @import stats
 #' @import utils
 #' @import shiny
+#' @export
 
 launch_photoBuild <- function(launch.browser = TRUE, ...) {
   fpath <- system.file("application", package = "photon")
